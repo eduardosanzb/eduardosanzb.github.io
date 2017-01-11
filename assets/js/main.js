27bbc438b3b9e181fc5978805d0185cf03eb3fd1
eduardosanzb <eduardosanzb@gmail.com>
2017-01-10 18:21:19 -0600

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,7 +55,7 @@ jQuery(document).ready(function($) {
     );
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    GitHubCalendar("#github-graph", "eduardosanzb");
+    GitHubCalendar(".github-graph", "eduardosanzb");
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
