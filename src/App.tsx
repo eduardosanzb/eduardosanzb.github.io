2e158bc271946ebc4868ce71503b97ee24ebdcab
eduardosanzbOptilyz <eduardo.sanchez@optilyz.com>
2018-04-16 23:50:02 +0200

    😐 Changed to Microsoft fabric components

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
 import * as React from "react";
-import "./App.css";
-
-import logo from "./logo.svg";
+import { DefaultButton } from "office-ui-fabric-react/lib/Button";
+import { Fabric } from "office-ui-fabric-react/lib/Fabric";
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+      <Fabric>
+        <DefaultButton>I am a button.</DefaultButton>
+      </Fabric>
     );
   }
 }
